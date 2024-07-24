76a171e15a4ad0a24a84481da088d3d2eec13cc0
Akash Sreedhar <99531114+akashsreedhar@users.noreply.github.com>
2024-07-24 14:22:40 +0530

    24072024004

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import instance from '../instance';
 
-function Banner(fetchUrl) {
+function Banner({fetchUrl}) {
     console.log("banner fetchUrl",fetchUrl)
+    const fetchData= async() => {
+        const result = await instance.get(fetchUrl)
+    }
   return (
     <div>Banner</div>
   )
