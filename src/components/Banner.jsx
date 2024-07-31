fbb0aaf25c0c9b121319d047deac8fa84c39f6ce
Akash Sreedhar <99531114+akashsreedhar@users.noreply.github.com>
2024-07-31 12:18:35 +0530

    background image added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect ,useState} from 'react';
 import instance from '../instance';
 
 function Banner({fetchUrl}) {
     console.log("banner fetchUrl",fetchUrl)
-    const [movie,setMovie]=useState()
+    const [movie,setMovie]=useState({})
+    const image_base_url="https://image.tmdb.org/t/p/original"
     const fetchData= async() => {
         const result = await instance.get(fetchUrl)
         const{data}=result;
@@ -18,7 +19,9 @@ console.log("testing")
 console.log("movie")
 
   return (
-    <div>Banner</div>
+    <div style={{height:'600px',backgroundImage: `url(${image_base_url}${movie?.backdrop_path})`}}>
+
+    </div>
   )
 }
 
