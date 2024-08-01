860cb24befe9e54f1338d0556cb334b619fe6921
Akash Sreedhar <99531114+akashsreedhar@users.noreply.github.com>
2024-08-01 13:57:10 +0530

    Trending section added

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,17 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import instance from '../instance'
-function Row({title,fetchUrl}) {
+import './Row.css'
+function Row({ title, fetchUrl }) {
   console.log(title)
   console.log(fetchUrl)
+  const image_base_url="https://image.tmdb.org/t/p/original"
 
-  const [allMovies, setAllMovies]=useState();
-  const fetchData=async()=>{
-    const response=await instance.get(fetchUrl);
+  const [allMovies, setAllMovies] = useState();
+  const fetchData = async () => {
+    const response = await instance.get(fetchUrl);
     console.log("response")
     console.log(response)
+    setAllMovies(response.data.results)
   }
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  console.log("All movies");
+  console.log("allmovies")
   return (
-    <div>Row</div>
+    <div className='row'>
+      <h2>{title}</h2>
+      <div className='movie_row'>
+        {
+          allMovies?.map(item=>(
+            <img className='movies' src={`${image_base_url}${item?.backdrop_path}`}  alt="no img"/>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
